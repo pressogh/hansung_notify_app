feb7796b02e96a6e63ac9a2e1a96c4e2b3adee84
KANO <user1205@naver.com>
2020-11-27 22:50:43 +0900

    BottomSheet & Calendar 적용

--- client/components/HomeCmp.js
+++ client/components/HomeCmp.js
@@ -15,29 +15,75 @@ import {
   Button,
 } from 'react-native';
 
-import FlipCard from 'react-native-flip-card';
-import Icon from 'react-native-vector-icons/EvilIcons';
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 const HomeCmp = ({ navigation, classdata }) => {
-    const renderitem = ({item, index}) => {
-        return (
-          <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('Class', { class_name: item.class_name })}>
-              <View style={styles.item}>
-                <Text style={styles.title}>{item.class_name}</Text>
-                <Text style={{flex: 1, flexWrap: "wrap"}}>{item.division}</Text>
-              </View>
-          </TouchableOpacity>
-        );
-    };
+  const renderBottomSheet = () => (
+    <View
+      style={{
+        paddingTop: 8,
+        paddingRight: 16,
+        paddingLeft: 16,
+        height: 320,
+        borderTopRightRadius: 20,
+        borderTopLeftRadius: 20,
+        alignItems: "center",
+      }}
+    >
+      <View style={{
+        backgroundColor: '#252525',
+        width: 200,
+        height: 10,
+        borderRadius: 20,
+        marginBottom: 20,
+      }} />
+      
+      <Calendar
+        onDayPress={(day) => {console.log('selected day', day)}}
+        style={{
+          width: 425,
+          height: 500,
+          borderRadius: 30,
+        }}
+      />
+    </View>
+  );
+
+  const sheetRef = React.useRef(null);
+
+  const renderitem = ({item, index}) => {
     return (
-        <View style={styles.container}>
-          <FlatList
-            data={classdata}
-            renderItem={renderitem}
-            keyExtractor={(item, index) => index.toString()}
-          />
-        </View>
+      <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('Class', { class_name: item.class_name })}>
+          <View style={styles.item}>
+            <Text style={styles.title}>{item.class_name}</Text>
+            <Text style={{flex: 1, flexWrap: "wrap"}}>{item.division}</Text>
+          </View>
+      </TouchableOpacity>
     );
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <FlatList
+          data={classdata}
+          renderItem={renderitem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={['50%', '3%']}
+        renderContent={renderBottomSheet}
+        initialSnap={1}
+        enabledContentTapInteraction={false}
+        enableSwipeMonths={true}
+      />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
